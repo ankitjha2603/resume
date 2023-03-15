6897654025e1d2a24c13917b62fd71a8ff3d96c6
Ankit kumar jha <ankitjha2603@gmail.com>
2023-03-15 19:50:27 +0530

    Update function.js

--- asset/js/function.js
+++ asset/js/function.js
@@ -1,33 +1,35 @@
 'use strict';
 
 function select(selection, singleElement = true) {
-    if (singleElement) {
-        return document.querySelector(selection)
-    } else {
-        return document.querySelectorAll(selection)
-    }
+	if (singleElement) {
+		return document.querySelector(selection)
+	} else {
+		return document.querySelectorAll(selection)
+	}
 }
+
 function append(mainElement, ...subElements) {
-    if(typeof("skjs")=="string"){
-        mainElement = select(mainElement)
-    }
-    subElements.forEach(subElement => {
-        mainElement.append(subElement);
-    })
+	if (typeof("skjs") == "string") {
+		mainElement = select(mainElement)
+	}
+	subElements.forEach(subElement => {
+		mainElement.append(subElement);
+	})
 }
+
 function newElement(cls, content = "", tag = "div") {
-    let element = document.createElement(tag);
-    if (cls) {
-        if (typeof cls === "object") {
-            cls.forEach(clsElement => {
-                element.classList.add(clsElement);
-            });
-        } else {
-            element.classList.add(cls);
-        }
-    }
-    if (content) {
-        element.innerText = content;
-    }
-    return element;
+	let element = document.createElement(tag);
+	if (cls) {
+		if (typeof cls === "object") {
+			cls.forEach(clsElement => {
+				element.classList.add(clsElement);
+			});
+		} else {
+			element.classList.add(cls);
+		}
+	}
+	if (content) {
+		element.innerText = content;
+	}
+	return element;
 }
